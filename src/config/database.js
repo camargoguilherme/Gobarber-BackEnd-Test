0057c0c7fcc85ff01fa1ef69c99fb4487a02b38d
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-04-04 15:38:49 -0300

    Adiconada configurações para uso do sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialetc: 'postgres',
+  host: 'localhost',
+  username: 'postgress',
+  password: 'postrgess',
+  database: 'gobarber',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
