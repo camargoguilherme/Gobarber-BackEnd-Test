491deb549eebf0889a2c08894e56a569906b44f7
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-04-06 13:07:08 -0300

    Adicionado envio de email com Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '12099896de731f',
+    pass: '3eb66fe625d298',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
